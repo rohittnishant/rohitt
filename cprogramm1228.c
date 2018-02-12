4c7038899bcd53c5e56ffb970fc866e1b0d2b0cf
rohittnishant <36066907+rohittnishant@users.noreply.github.com>
2018-02-12 16:37:24 +0530

    Create cprogramm1228.c

---
+++ cprogramm1228.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<conio.h>
+int main() 
+{
+    int j,a[10],i;
+    scanf("%d",&n);
+    for(i=0;i<j;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<j;i++)
+    {
+      printf("%d %d\n",a[i],i);
+    }
+	return 0;
+}
